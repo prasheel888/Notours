c4569e5f6fdadbfb196103d354d174426af88716
divo888 <prasheelchadotra88@gmail.com>
2021-06-27 16:29:57 +0530

    master

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCGzFrVG2hLrl-QMG-aMMbTT4RvbJ90waA",
+    authDomain: "notours-68111.firebaseapp.com",
+    projectId: "notours-68111",
+    storageBucket: "notours-68111.appspot.com",
+    messagingSenderId: "893573454922",
+    appId: "1:893573454922:web:a937c4113ee0e2d766dab3",
+    measurementId: "G-FY6H844RSM"
+  };
